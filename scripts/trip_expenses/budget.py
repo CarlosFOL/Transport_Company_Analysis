a8fa7014051de6ce3de0b11f9af2eabccfdeda0d
CarlosFOL <carlosdsscience@gmail.com>
2024-03-09 19:32:26 +0100

    Budget: We dealt with those trips that have Unregistered values in Transfer Date column

--- scripts/trip_expenses/budget.py
+++ scripts/trip_expenses/budget.py
@@ -5,30 +5,62 @@ class Budget:
     Amount of money that a driver receives 
     for his trip expenses
     """
+    
+    
     def __init__(
             self, df_tc: pd.DataFrame, 
             transf_date: bool = False):
-        cond = df_tc['FECHA TRANSFERENCIA'] == 'Unregistered'
-        if transf_date:
-            self._df_tc = df_tc[~cond]
-        else:
-            self._df_tc = df_tc[cond]
+        self._transf_date = transf_date
+        self._df_tc = df_tc.copy()
+    
+    
+    @property
+    def transf_date(self) -> bool:
+        """
+        It indicates whether to return the 
+        trips with a valid transfer date.
+        """
+        return self._transf_date
+    
+    
+    @property
+    def table_tc(self) -> pd.DataFrame:
+        """
+        The table that records the main attributes of the 
+        trips made by the Anonymous Transport Company.
+        """
+        return self._df_tc
+    
+    @table_tc.setter
+    def table_tc(self, df_tc_up: pd.DataFrame) -> None:
+        """
+        Update the table_tc contents after 
+        manipulating the data in Budget column
+        """
+        self._df_tc = df_tc_up
         
+
     @property
     def budget_table(self) -> pd.DataFrame:
         """
-        It contains the information about the 
-        budget of those trips that meet the 
-        previous condition.
+        It contains the budget data of these trips 
+        that meet the transfer date condition.
         """
-        return self._df_tc
+        cols = ['FECHA TRANSFERENCIA', 'ORIGEN', 'DESTINO', 
+                'FAMILIA', 'NUEVO / SEMINUEVO', 'POR RENDIR S/']
+        cond = self.table_tc['FECHA TRANSFERENCIA'] == 'Unregistered'
+        if self.transf_date:
+            return self.table_tc.loc[~cond, cols]
+        else:
+            return self.table_tc.loc[cond, cols]
+    
     
     def dtypes(self, dtype: object = None) -> pd.Series:
         """Dtypes in Budget
         
         It can show either what types of data 
         are in the budget column or the kind of
-        values that exist for a particular dtype'
+        values that exist for a particular dtype.
         """
         budget_dtypes = self.budget_table['POR RENDIR S/'].map(type)
         if dtype == None:
--- scripts/trip_management/date_types/datetimes/datetimes_obj.py
+++ scripts/trip_management/date_types/datetimes/datetimes_obj.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import pandas as pd
 from ..dtypes import DateTypes
-# from .trip_duration import TripDuration
 
 class DatetimesObj(DateTypes):
     
@@ -41,40 +40,3 @@ class DatetimesObj(DateTypes):
             return self.dt_table.loc[consistent_cond, :]
         else:
             return self.dt_table.loc[~consistent_cond, :]
-    
-    # @property
-    # def trip_duration(self) -> pd.DataFrame:
-    #     """
-    #     Get the table with the average 
-    #     trip duration of all the routes
-    #     """
-    #     df_trip_duration = TripDuration(self.filter_by_relationship())
-    #     return df_trip_duration.calculate_trip_times()
-    
-    # def find_trip_duration(self, df_trips: pd.DataFrame) -> pd.DataFrame:
-    #     """
-    #     Find the trip duration of 
-    #     specific routes
-    #     """
-    #     routes = df_trips.apply(lambda x: f"{x['ORIGEN']} - {x['DESTINO']}", axis = 1)
-    #     routes.name = 'Routes'
-    #     trip_durations = pd.merge(
-    #         left = self.trip_duration, right = routes, 
-    #         left_index = True, right_on='Routes'
-    #         )     
-    #     return trip_durations.iloc[:, [1, 0]]
-    
-    # def get_correct_arr_date(self, df_trips: pd.DataFrame) -> pd.Series:
-    #     """
-    #     Get the correct arrival dates by adding 
-    #     the departure dates with the trip durations
-    #     """
-    #     routes_duration = self.find_trip_duration(df_trips)['Trip_Duration']
-    #     arrival_dates = (
-    #         pd.to_datetime(
-    #             self.dt_table.loc[routes_duration.index, 'FECHA DE INICIO']
-    #             ) + pd.to_timedelta(
-    #             routes_duration
-    #             )
-    #         )
-    #     return arrival_dates
